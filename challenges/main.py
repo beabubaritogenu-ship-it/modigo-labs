0b2f0138d263c10c031ade5633387a8f702b5e76
beabubaritogenu-ship-it <beabubaritogenu@gmail.com>
2026-09-08 17:05:29 +0100

    Complete: Split a Bill

--- challenges/main.py
+++ challenges/main.py
@@ -1,12 +1,10 @@
-def fizzbuzz_counts(n):
-    counts = {"fizz": 0, "buzz": 0, "fizzbuzz": 0}
-    
-    for number in range(1, n + 1):
-        if number % 3 == 0 and number % 5 == 0:
-            counts["fizzbuzz"] += 1
-        elif number % 3 == 0:
-            counts["fizz"] += 1
-        elif number % 5 == 0:
-            counts["buzz"] += 1
+def split_bill(bill_amount, tip_percent, people):
+    tip_amount = bill_amount * (tip_percent / 100)
+    grand_total = bill_amount + tip_amount
+    persons_share = grand_total / people
+    persons_share = round(persons_share, 2)
+    return persons_share
 
-    return counts
+split_bill(100, 10, 2)
+split_bill(60, 20, 3)
+split_bill(50, 0, 1)
