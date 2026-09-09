e1ce2939be4367efb7a87751b4afabb8b306ff58
beabubaritogenu-ship-it <beabubaritogenu@gmail.com>
2026-09-09 13:23:43 +0100

    Complete: Write a function that takes two coordinate points (as tuples

---
+++ labs/main.py
@@ -0,0 +1,9 @@
+def same_point(point1, point2):
+    if point1 == point2:
+        return True
+
+    else:
+        return False
+same_point((1,2), (1, 2))
+same_point((1, 2), (2, 1))
+same_point((0, 0), (0, 0))
