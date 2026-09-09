3ce15c13d687a40b591a934e65047a597303e551
beabubaritogenu-ship-it <beabubaritogenu@gmail.com>
2026-09-09 13:43:48 +0100

    Complete: The function below is meant to combine two sets of tags into

--- labs/main.py
+++ labs/main.py
@@ -1,11 +1,3 @@
-def count_unique_visitors(visitors):
-    results = []
-    for visitor in visitors:
-        if visitor not in results:
-            results.append(visitor)
-    return len(results)
-
-count_unique_visitors(["Ada", "Bola", "Ada"])
-count_unique_visitors([])
-count_unique_visitors(["Chidi"])
-count_unique_visitors(["Ada", "Ada", "Ada"])
+def merge_tags(tags1, tags2):
+    merged = tags1 | tags2
+    return merged
