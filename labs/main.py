1843ce46ecffdeca5b8a8e2796f656718022261c
beabubaritogenu-ship-it <beabubaritogenu@gmail.com>
2026-09-09 14:54:26 +0100

    Complete: Write a function that takes a two-element tuple and returns

--- labs/main.py
+++ labs/main.py
@@ -1,6 +1,3 @@
-def has_all_vowels(word):
-    required = {"a", "e", "i", "o", "u"}
-    word = word.lower()
-    # TODO: build a set of vowels actually found in `word`,
-    # then check if it contains all of `required`
-    return required <= set(word)
+def swap_tuple(pair):
+    # TODO: return a new tuple with pair's two elements swapped
+    return pair[1], pair[0]
